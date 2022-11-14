3f9424e7489b2aa3f592b5c26d44c76a1057a9e4
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-11-14 18:39:15 +0200

    .

--- OOPLesson7/Program.cs
+++ OOPLesson7/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OOPLesson7;
+using OOPLesson7.Task;
 using System;
 
 namespace MyApp // Note: actual namespace depends on the project name.
@@ -8,10 +9,42 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             //Person person1 = new Person("yarin");//error compiler
-            Person person2 = new Students(30, "yarin");//good
-            Console.WriteLine(person2.GetString());
-  
+            //Person person2 = new Students(30, "yarin");//good
+            //Console.WriteLine(person2.GetString());
+            Shape circle = new Circle(2.6);
+            Console.WriteLine(circle.GetArea());
+            Shape line = new Line(3.5);
+            Console.WriteLine(line.GetArea());
+            //להדפיס את GetArea
+            //shape c = new Line();
 
+            //ליצור מחלקה  shape  abstract
+
+            //ליצור פונקציה GetArea .abstract //סוג  double
+
+            //מחלקה circle  שיורשת מshape
+
+            //prop:
+            //radius double
+
+            // ליצור פונקציה של GetArea ovrride
+
+
+            //ליצור מחלקה Line  שיורשת מshape
+            //
+            // prop
+            // x double
+            //
+
+            // ליצור פונקציה של GetArea ovrride 
+            //
+
+            //main
+
+            //shape b = new circle();
+            //להדפיס את GetArea
+            //shape c = new Line();
+            //להדפיס את GetArea
 
 
         }
---
+++ OOPLesson7/Task/Line.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLesson7.Task
+{
+    internal class Line : Shape
+    {
+        public double X { get; set; }
+
+        public Line(double x)
+        {
+            X = x;
+        }
+
+        public override double GetArea()
+        {
+            return X;
+        }
+    }
+}
